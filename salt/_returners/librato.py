c241f81cc376b434a0e56a6b0785a392e002bbf2
Peter Baumgartner <pete@lincolnloop.com>
2013-09-19 21:33:23 -0500

    Make sure dict key is a string

--- salt/_returners/librato.py
+++ salt/_returners/librato.py
@@ -27,7 +27,7 @@ def _flatten_and_collect_floats(obj, base=None):
     flattened = {}
     # convert list to dictionary
     if isinstance(obj, list):
-        obj = dict([pair for pair in enumerate(obj)])
+        obj = dict([(str(pair[0]), pair[1]) for pair in enumerate(obj)])
 
     for key, item in obj.items():
         key = base and '.'.join([base, key]) or key
